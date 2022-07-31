f6994facb7a334c46884aaddd012843ecc418ab2
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-07-31 13:28:06 +0700

    main runner class

--- WebScrapingAndSaveToDB/src/test/java/com/shopee/getProduct.java
+++ WebScrapingAndSaveToDB/src/test/java/com/shopee/getProduct.java
@@ -1,5 +1,26 @@
 package com.shopee;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/*
+ * method get product
+ */
 public class getProduct {
+
+	static WebDriver driver;
+
+	public getProduct() {
+		getProduct.setUp();
+
+	}
+
+	private static void setUp() {
+		System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		driver.get("https://shopee.co.id");
+	}
 
 }
---
+++ WebScrapingAndSaveToDB/src/test/java/com/shopee/mainRunner.java
@@ -0,0 +1,11 @@
+package com.shopee;
+
+/*
+ * main method
+ */
+public class mainRunner {
+	public static void main(String[] args) {
+		getProduct trial = new getProduct();
+	}
+
+}
